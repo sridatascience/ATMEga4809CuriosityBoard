9e5a02a43ff1d8148254dda957f3d635d2022fc0
Sridhar Vumma <sridhar6358@gmail.com>
2023-01-23 19:10:13 +0530

    Remodified the code to solve make file issues

---
+++ ATMEGA4809.X/build/default/production/newmain.o.d
@@ -0,0 +1,43 @@
+build/default/production/newmain.o.d build/default/production/newmain.o: \
+ newmain.c c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\features.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\bits\alltypes.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\bits\stdint.h \
+ C:/Program\ Files/Microchip/MPLABX/v6.05.02.6747/packs/Microchip/ATmega_DFP/3.1.263/xc8/avr/include/avr/iom4809.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\xmega.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\lock.h
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\features.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\bits\alltypes.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\bits\stdint.h:
+
+C:/Program\ Files/Microchip/MPLABX/v6.05.02.6747/packs/Microchip/ATmega_DFP/3.1.263/xc8/avr/include/avr/iom4809.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v2.40\avr\avr\include\avr\lock.h:
